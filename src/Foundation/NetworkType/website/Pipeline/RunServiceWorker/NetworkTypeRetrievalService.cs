d76ec640cad20b0a49802e4a79e059632267865b
NagendraVIN1347 <Nagendrakumarb.vin1347@outlook.com>
2021-09-15 12:50:40 +0530

    initial commit

---
+++ src/Foundation/NetworkType/website/Pipeline/RunServiceWorker/NetworkTypeRetrievalService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VerticurlPoc.Foundation.NetworkType.Pipeline.RunServiceWorker
+{
+    public class NetworkTypeRetrievalService
+    {
+        public static string RetrieveNetworkType()
+        {
+            var context = HttpContext.Current;
+            string[] header = context.Request.Headers.AllKeys;
+            if (header.Contains("network-type"))
+            {
+                return context.Request.Headers.Get("network-type");
+            }
+            return string.Empty;
+        }
+
+            
+    }
+}
---
+++ src/Foundation/NetworkType/website/Pipeline/RunServiceWorker/RunServiceWorkerPipeline.cs
@@ -0,0 +1,54 @@
+﻿using Sitecore;
+using Sitecore.Data.Items;
+using Sitecore.Pipelines.HttpRequest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VerticurlPoc.Foundation.NetworkType.Pipeline.RunServiceWorker
+{
+    public class RunServiceWorkerPipeline : HttpRequestProcessor
+    {
+        public override void Process(HttpRequestArgs args)
+        {
+            HttpContext currentHttpContext = HttpContext.Current;
+
+            if (currentHttpContext == null || Context.Database == null)
+                return;
+            if (Context.Site.Name.ToLower() == "shell")
+                return;
+
+
+            var networkType = NetworkTypeRetrievalService.RetrieveNetworkType();
+            var slowNetork = Sitecore.Configuration.Settings.GetSetting("SlowNetwork");
+            //var fastNetork = Sitecore.Configuration.Settings.GetSetting("FastNetwork");
+            if (string.IsNullOrEmpty(networkType))
+            {
+                currentHttpContext.Response.Redirect(currentHttpContext.Request.Url.Scheme + "://" + currentHttpContext.Request.Url.Host + "/redirect.html");
+            }
+            else if(slowNetork.Contains(networkType))
+            {
+                SetDevice("SlowNetwork");
+                return;
+            }
+            //else if (fastNetork.Contains(networkType))
+            //{
+            //    SetDevice("Defult");
+            //    return;
+            //}
+            else
+            {
+                return;
+            }
+
+
+        }
+        private void SetDevice(string deviceName)
+        {
+            DeviceItem device = Context.Database.Resources.Devices["Default"];
+            device = Context.Database.Resources.Devices[deviceName];
+            Context.Device = device;
+        }
+    }
+}
